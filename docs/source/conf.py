97a559183ff51fd143f9e09d58223afeaa963474
Spencer McIntyre <zeroSteiner@gmail.com>
2015-05-21 09:28:06 -0400

    Fix RTD docs theme and travis sphinx warnings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -106,7 +106,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'classic'
+if on_rtd:
+	html_theme = 'default'
+else:
+	html_theme = 'classic'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
