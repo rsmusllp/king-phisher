7ce9f53409fb9ba7a3fd1193fc9fd43e05365d76
Spencer McIntyre <zeroSteiner@gmail.com>
2015-07-15 12:34:45 -0400

    Bump the version to 0.3.0 and update pip requirements

--- king_phisher/version.py
+++ king_phisher/version.py
@@ -32,10 +32,10 @@
 
 import collections
 
-version_info = collections.namedtuple('version_info', ['major', 'minor', 'micro'])(0, 2, 1)
+version_info = collections.namedtuple('version_info', ['major', 'minor', 'micro'])(0, 3, 0)
 """A tuple representing the version information in the format ('major', 'minor', 'micro')"""
 
-version_label = ''
+version_label = 'alpha'
 """A version label such as alpha or beta."""
 version = "{0}.{1}.{2}".format(version_info.major, version_info.minor, version_info.micro)
 """A string representing the full version information."""
