4a170403446e12fbb003e023469218b2fa025635
jacobj10 <jacobj10@bbhcsd.org>
2016-06-22 15:41:45 -0400

    Created skeleton GUI for Campaign Comparison, added initializing from Tools and campaign compare class

---
+++ king_phisher/client/windows/compare_campaigns.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  king_phisher/client/windows/rpc_terminal.py
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are
+#  met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following disclaimer
+#    in the documentation and/or other materials provided with the
+#    distribution.
+#  * Neither the name of the project nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+#  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+#  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+#  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+#  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+#  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
+
+import logging
+import os
+import signal
+import sys
+
+from king_phisher import find
+from king_phisher import json_ex
+from king_phisher import utilities
+from king_phisher import version
+from king_phisher.client import client_rpc
+from king_phisher.client import dialogs
+from king_phisher.client import gui_utilities
+
+from gi.repository import GLib
+from gi.repository import Gtk
+
+try:
+	from gi.repository import Vte
+except ImportError:
+	has_vte = False
+	"""Whether the :py:mod:`Vte` module is available or not."""
+else:
+	has_vte = True
+
+class CampaignCompWindow(gui_utilities.GladeGObject):
+	dependencies = gui_utilities.GladeDependencies(
+		children=(
+			'notebook_main',
+			'box_internal',
+			'box_info',
+			'label_status',
+			'checkbutton_expire',
+			'scrolledwindow',
+			'label_list',
+			'label_graph'
+		),
+
+	)
+	top_gobject = 'window'
+	def __init__(self, *args, **kwargs):
+		super(CampaignCompWindow, self).__init__(*args, **kwargs)
+		self.window.show()
+
