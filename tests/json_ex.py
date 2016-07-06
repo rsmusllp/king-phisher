1e5678a38ba2540bfd8a31a3921485b6103a88f2
Spencer McIntyre <zeroSteiner@gmail.com>
2016-07-06 14:06:27 -0400

    Add json_ex unit tests to the proper file

--- tests/json_ex.py
+++ tests/json_ex.py
@@ -1,17 +1,18 @@
-import unittest
-import datetime
-
 from king_phisher import testing
 from king_phisher import json_ex
 
 class JsonExTests(testing.KingPhisherTestCase):
-	def test_dumps(self):
-		self.assertIsInstance(json_ex.dumps('test'), basestring)
-		self.assertIsInstance(json_ex.dumps(True), basestring)
-		self.assertIsInstance(json_ex.dumps(100), basestring)
+	def test_json_ex_dumps(self):
+		self.assertIsInstance(json_ex.dumps('test'), str)
+		self.assertIsInstance(json_ex.dumps(True), str)
+		self.assertIsInstance(json_ex.dumps(100), str)
 
-	def test_loads(self):
+	def test_json_ex_loads(self):
 		try:
-			 json_ex.loads(json_ex.dumps("test"))
+			json_ex.loads(json_ex.dumps('test'))
 		except ValueError:
 			self.fail('Invalid data type for json_ex.loads()')
+
+	def test_json_ex_loads_invalid(self):
+		with self.assertRaises(ValueError):
+			json_ex.loads("'test")
