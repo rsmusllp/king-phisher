28f516917c510ca946235dbb888c8598c55b90ab
Spencer McIntyre <zeroSteiner@gmail.com>
2017-08-25 14:46:24 -0400

    Create a new tools/development folder

--- tools/development/cx_freeze.py
+++ tools/development/cx_freeze.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  tools/cx_freeze.py
+#  tools/development/cx_freeze.py
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
--- tools/development/test_runner.py
+++ tools/development/test_runner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
-#  tools/test_runner.py
+#  tools/development/test_runner.py
 #
 #  Redistribution and use in source and binary forms, with or without
 #  modification, are permitted provided that the following conditions are
